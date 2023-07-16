055c1d864cfe105dadabe1446ec8a365c9702505
zhongjinggz <zhongjinggz@163.com>
2023-07-16 14:06:18 +0800

    minor adjustment

--- unjuanable/src/main/java/demo/unjuanable/adapter/driven/persistence/orgmng/EmpRepositoryJdbc.java
+++ unjuanable/src/main/java/demo/unjuanable/adapter/driven/persistence/orgmng/EmpRepositoryJdbc.java
@@ -3,7 +3,6 @@ package demo.unjuanable.adapter.driven.persistence.orgmng;
 import demo.unjuanable.domain.common.valueobject.Period;
 import demo.unjuanable.domain.orgmng.emp.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
