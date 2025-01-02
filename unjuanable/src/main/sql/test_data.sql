05f50a2f7e719dffbf61e36b45ebe2e8552e97c6
Zhong Jing <zhongjinggz@163.com>
2025-01-02 16:42:16 +0800

    run unit tests and integration tests with Maven

--- unjuanable/src/main/sql/test_data.sql
+++ unjuanable/src/main/sql/test_data.sql
@@ -1,11 +1,11 @@
 -- tenant
-delete from tenant;
+delete from tenant where 1 = 1;
 
 insert into tenant (id, name, status_code, created_at, created_by)
 values (1, "梁山", "EF", now(), 1);
 
 -- org_type
-delete from org_type;
+delete from org_type where 1 = 1;
 
 insert into org_type (tenant_id, code, name, status_code, created_at, created_by )
 values (1, "ENTP","企业", "EF", now(), 1);
@@ -20,7 +20,7 @@ insert into unjuanable.org_type (tenant_id, code, name, status_code, created_at,
 values (1, "DIRDEP","直属部门", "EF", now(), 1);
 
 -- org
-delete from org;
+delete from org where 1 = 1;
 
 insert into org (id, tenant_id, org_type_code, superior_id, name, leader_id, status_code, created_at, created_by)
 values (1, 1, "ENTP", -1, "梁山科技有限公司", 1, "EF", now(), 1);
@@ -34,7 +34,7 @@ values (1, 1, 1, "20020001","440101197001011234","宋江","M", "1970-01-01", "RE
 -- emp_num_counter
 delete from emp_num_counter where 1 = 1;
 insert into emp_num_counter (tenant_id, year_num, max_emp_num)
-values(1, 2023, 1);
+values(1, 2023, 1), (1, 2024, 1), (1, 2025, 1), (1, 2026, 1);
 
 delete from skill_type where 1 = 1;
 insert into skill_type (tenant_id, id, name, created_at, created_by)
